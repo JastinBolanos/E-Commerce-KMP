798721a119dfeb8281b86e846ad6bbc0efb488ab
jastin <jastinbolanos@gmail.com>
2026-07-04 17:10:44 -0500

    chore: remove network dependencies (Ktor) to strictly serve local mock resources

--- composeApp/src/wasmJsMain/kotlin/com/remedioz/natura/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package com.remedioz.natura
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
--- composeApp/src/wasmJsMain/kotlin/com/remedioz/natura/presentation/components/BackHandler.js.kt
+++
@@ -1,7 +0,0 @@
-package com.remedioz.natura.presentation.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun BackHandler(isEnabled: Boolean, onBack: () -> Unit) {
-}
