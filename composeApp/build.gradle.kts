63eb0b8f19cf72f4f52fb960cdeb0665ed2ab062
jastin <jastinbolanos@gmail.com>
2026-07-22 17:04:30 -0500

    build: incrementar versionCode a 3 y versionName a 1.2.0
    - Actualizada la configuración de compilación de Android para el nuevo release (v1.2.0).
    - Preparación del entorno para la distribución de la nueva versión con mejoras de UI/UX y localización.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -67,8 +67,8 @@ android {
         applicationId = "com.ecommerce.kmp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
     packaging {
         resources {
